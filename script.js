bf425d97a701066a05cc84cbc2570bc9c2229854
ahmad-Imran473 <muhammadahmadxd473@gmail.com>
2026-09-13 01:05:31 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const links = document.querySelectorAll("nav a");
+
+    links.forEach(function (link) {
+        link.addEventListener("click", function () {
+            console.log("Navigation clicked");
+        });
+    });
+
+});
